e19c0e4c795c60ecfa7873a1ca4659183934108c
yoloboyyy <46230065+yoloboyyy@users.noreply.github.com>
2018-12-29 01:17:10 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+hamburger.onclick=()=>{
+    var x=document.getElementById('myNavBar')
+    if(x.className==='navBar'){
+        x.className+=' responsive';
+    }else
+    {
+        x.className='navBar';
+    }
+}
+
+
+
+
